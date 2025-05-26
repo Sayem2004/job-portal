e0b89981fcfdb768015d87605b6afb22e64ca643
Sayem2004 <mdalimransayem@gmail.com>
2025-05-26 22:00:44 +0600

    Changes design

--- View/CareerResources.php
+++ View/CareerResources.php
@@ -18,23 +18,24 @@ if (!isset($_SESSION['name'])) {
     </div>
     <center>
     <fieldset style="width: 700px; background-color: antiquewhite; padding: 20px; border-radius: 8px;">
-        <h1>Career Resources</h1>
+        <h1 style="text-align: left;">Career Resources</h1>
         
-        <h3>Articles & Guides</h3>
-        <ul>
-            <li><a href="#" target="_blank">How to Write a Professional Resume</a></li>
-            <li><a href="#" target="_blank">Top 10 Interview Questions and Answers</a></li>
-            <li><a href="#" target="_blank">Effective Salary Negotiation Tips</a></li>
+        <h3 style="text-align: left;">Articles & Guides</h3>
+        <ul style="text-align: left;">
+            <li><a href="https://resumebuild.com/" >How to Write a Professional Resume</a></li>
+            <li><a href="https://www.indeed.com/career-advice/interviewing/top-20-interview-questions" >Top 10 Interview Questions and Answers</a></li>
+            <li><a href="https://online.hbs.edu/blog/post/salary-negotiation-tips">Effective Salary Negotiation Tips</a></li>
         </ul>
 
-        <h3> Video Tutorials</h3>
-        <ul>
-            <li><a href="https://www.youtube.com/watch?v=2XvZtW1-PpY" target="_blank">Resume Building (YouTube)</a></li>
-            <li><a href="https://www.youtube.com/watch?v=8-S0RCDS5jg" target="_blank">Mastering Interview Skills</a></li>
+        <h3 style="text-align: left;"> Video Tutorials</h3>
+        <ul style="text-align: left;">
+            <li><a href="https://www.youtube.com/watch?v=XJ7bYdjKDcA">Resume Building (YouTube)</a></li>
+            <li><a href="https://www.youtube.com/watch?v=ZMByWenSRdI" target="_blank">Mastering Interview Skills</a></li>
+            <!-- target="_blank" ------ Use for new tab -->
         </ul>
 
-        <h3>Downloadable Templates</h3>
-        <ul>
+        <h3 style="text-align: left;">Downloadable Templates</h3>
+        <ul style="text-align: left;">
             <li><a href="sample_resume.pdf" download>Download Sample Resume (PDF)</a></li>
             <li><a href="interview_checklist.docx" download>Download Interview Checklist (DOCX)</a></li>
         </ul>
